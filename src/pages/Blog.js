b480b9a5ab4343d86d44b6ebf242051b9ced851f
jams <jamesqiyang@gmail.com>
2021-11-22 16:45:59 -0500

    added another blog post

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -6,18 +6,19 @@ const Blog = () => {
     <div className="flex flex-col flex-grow
     max-h-full items-center
     overflow-auto p-1.5 fade-in">
+      <Post3 />
       <Post2 />
       <Post1 />
     </div>
   )
 };
 
-const Post = ({ id, date, title, text }) => {
+const Post = ({ id, date, title, body }) => {
   return (
     <div className="card xl:min-w-full">
       <h2 className="my-1 py-2 text-center align-middle justify-center dark:text-white text-xl font-bold" >Post {id}</h2>
-      <span className="m-2 p-2 px-auto dark:text-white">{title}</span>
-      <span className="m-2 p-2 px-auto dark:text-white">{text}</span>
+      <span className="m-2 p-2 px-auto dark:text-white text-lg">{title}</span>
+      <span className="m-2 p-2 px-auto dark:text-white">{body}</span>
       <div className="flex flex-grow flex-1 
         w-full px-4 py-1 text-black justify-center bg-red-300 rounded-b-3xl ">{date}</div>
     </div>
@@ -33,7 +34,7 @@ Post.propTypes = {
 
 const Post1 = () => {
   return (
-      <Post id={1} date={"Nov 19 2021"} title={"Step 1: Learning the game"} text={<>
+      <Post id={1} date={"Nov 19 2021"} title={"Step 1: Learning the game"} body={<>
         There's an interesting phenomenon when someone is first introduced to board games.
         Players are first flooded with rules and instructions. They have to learn almost everything about the game just to start, let alone have fun.
         It's weird because most (video) games gradually introduce more mechanics and gameplay elements as more time is spent in the game.
@@ -63,7 +64,7 @@ const Post1 = () => {
 
 const Post2 = () => {
   return (
-      <Post id={2} date={"Nov 21 2021"} title={"Step 2: Having Fun"} text={<>
+      <Post id={2} date={"Nov 21 2021"} title={"Step 2: Having Fun"} body={<>
         I think the number one reason why people play board games is because it is fun. One of my favourite quotes is from Reggie, when he said "If it's not fun, why bother".
         This perfectly describes any form of games that we play on the table. We gather around and enjoy the time we spend at that occasion. Even if the game itself is not fun
         (looking at you Monopoly), the game can still act as a catalyst for people to enjoy each other's company.
@@ -87,6 +88,68 @@ const Post2 = () => {
         <br />
         Until then, have fun with your cardboard :)
       </>} />)
+}
+
+const Post3 = () => {
+  return (
+    <Post id={3} date={"Nov 22 2021"} title={"Sleeving cards"} body={<>
+      If you have never seen a sleeve for a card before, they are plastic coverings for the cards that is meant to protect your cards.
+      There are plenty of types and size so I'll give a brief introduction to these useful things.<br/>
+      <br/>
+      <h3>0. Prelude </h3>
+      In my area (Canada), sleeves for the most common size and the most common type goes for about 2 Cad per 50 sleeves. Some are more expensive, some are less.
+      The important thing to know is that they are not always necessary. They may help with shuffling and keeping cards undamaged, but the limiting factor is still the price point.
+      A game that contains 200 cards can go for $25 and I think that it's very reasonable for that value. However, if you add sleeve prices on top of the base price tag, then it becomes $33.
+      That is almost a 33% increase in price. If you want get more premium sleeves, that percentage goes up even further. PLEASE, always reconsider if you need sleeves or not.
+      As a guy who sleeves almost all his games, I strongly encourage you to think about the following aspects before purchasing sleeves: <br/>
+      <ul> 
+      <li>- How often are you going to play this game?</li>
+      <li>- What is your budget?</li>
+      <li>- Can you replace the cards easily?</li>
+      <li>- Can your cards hold up to long term use?</li>
+      <li>- Where are you going to play and how sweaty are you/your friends' hands (I'm serious about this one)?</li>
+      </ul>
+      <br/>
+
+      <h1>1. Size of your Cards and Size of your Sleeves </h1>
+      If the size of your cards and sleeves don't match, then the sleeves loses most of it's function. Make sure you know what your card sizes are and which sleeve size you are going to buy.
+      There are plenty of online resources that will help you with this. Main ones I use are Board Game Geek and Sleeve Your Games.
+      <br/>
+      The most common size for cards is 'Standard Sized' (2.5" x 3.5"). This size is used for your poker decks and common card games like Pokemon and MTG.
+      <br/>
+      After standard size, you have 'Standard American' (2-1/4" x 3.5"), which is also commonly used for games. This is also called the japanese size for games like YuGiOh.
+      <br/>
+      There are also more niche cards sizes such as Tarot, Small, Mini, Square, and so on.
+      <br/><br/>
+
+      <h3>2. Backs and Textures </h3>
+      There are 2 main finishes; glossy and matte (think normal glass and frosted glass).
+      For board games, most people tend to enjoy the glossy finish with transparent backs. This allows the artwork and text to be seen easily. 
+      Other people will choose to use sleeves with a solid colour back to differentiate different card types.
+      There are also card backs that have graphic images or textured finishes, often offered by kick starter exclusives or third-party company (Anime backs are very popular).
+      I prefer the matte finishing for cards, as it feels much better in the hand. These type of sleeves usually come with a premium price. Use wisely (aka, use it on your prized possessions).
+      <br/><br/>
+
+      <h3>3. Double Sleeving and Triple Sleeving (Mainly for TCG and CCG)</h3>
+      This is reserved for the most elite of protective gamers. The practice is to use a inner sleeve with an larger outer sleeve (and maybe even a third sleeve) for one card.
+      To maximize the protection, the direction of the sleeves are alternating. If the card was inserted into the inner sleeve from the top, then the next sleeve will be inserted from the bottom.
+      People do this to protect their cards and often their graphic back sleeves. Mainly seen in TCG communities where one card cost hundreds.
+      I've not done this before because it just makes your deck like 4 times thicker.
+      <br/><br/>
+
+      <h3>4. Buying</h3>
+      Amazon is not always the best place to buy. Your local shop that carries sleeves will usually be priced better than large online retailers. 
+      It will also help your local business, which is always a good thing. If you are unsure about your card sizes, bring it to the shop and match it in person (if able).
+      Buying in bulk also will help reduce the price, but that will depend on your situation.<br/>
+      Brands are do make a difference in buying because of the pricing and the quality that they offer. 
+      I buy Mayday for cheap and quantity, Ultimate Guards for quality, Ultra Pros for matte fronts and coloured backs, and Dragon Shield for premium textured matte.
+      I've also tried many other brands and all of them have a purpose they fulfill. Bad sleeves do exist and you need some experience with the product line to know (aka buying and trying them).
+      <br/><br/>
+
+      I think I still have a bit more to say about sleeving and what they can do for the games, but I've written plenty already. Maybe in the future I will come back to this topic.
+      <br/>
+      Until then, have fun with your cardboard :)
+    </>} />)
 }
 
 export default Blog;
